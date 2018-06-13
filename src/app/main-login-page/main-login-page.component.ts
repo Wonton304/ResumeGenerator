8caa627316d98d020fece1f23c3e59d5d5b1d8f9
amymao1 <mao.amy1@gmail.com>
2018-06-12 22:24:21 -0700

    main login page added

--- src/app/main-login-page/main-login-page.component.ts
+++ src/app/main-login-page/main-login-page.component.ts
@@ -7,9 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainLoginPageComponent implements OnInit {
 
-  constructor() { }
+  applicantIsHidden:boolean;
+  employerIsHidden:boolean;
+  constructor() {
+    this.applicantIsHidden = false;
+    this.employerIsHidden = false;
+   }
 
   ngOnInit() {
   }
+
+  revealHideApplicant() {
+    this.applicantIsHidden = !this.applicantIsHidden;
+  }
+
+  revealHideEmployer() {
+    this.employerIsHidden = !this.employerIsHidden;
+  }
+
+
+//   function revealHideApplicant() {
+//     var x = document.getElementById("applicant-login-box");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
 
 }
