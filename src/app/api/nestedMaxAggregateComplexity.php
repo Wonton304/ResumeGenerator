d8fa9ef7a4541fdf795c1752f0cfa4b16e2bb4f1
amymao1 <mao.amy1@gmail.com>
2018-06-17 01:02:56 -0700

    Insert-Hobbies.php is gucci. AggregateComplexity, MinProficiency, division, Join, nestedMax/Min are work in progress, dunno every will be needed

---
+++ src/app/api/nestedMaxAggregateComplexity.php
@@ -0,0 +1,33 @@
+<?php
+ header("Access-Control-Request-Method: *");
+  header("Access-Control-Request-Headers: *");
+  header("Access-Control-Allow-Origin: *");
+// Create connection to Oracle
+$conn = oci_connect("ora_f5x0b", "a40858169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+
+
+$complexity= 'SELECT max(avg(complexity)) FROM CodingProject1 Group by category';
+$stid = oci_parse($conn, $complexity);
+$result = oci_execute($stid);
+//to remember the entire json string
+$json = array(); //hailin
+
+// Fetch each row in an associative array
+//print '<table border="1">'
+
+while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+
+     //to concatenate a ',' after every json object. used to split before parsing.
+	    $json [] = $row ;
+
+}
+$response = (json_encode($json));
+
+
+echo $response;
+
+// Fetch each row in an associative array
+
+//0cy27
+?>
