58ac2ab4fa6a5e0ed5b916863868371162bc2eb0
anlinpanda <anlin_c@yahoo.ca>
2018-06-17 03:20:31 -0700

    changed insertHobbies

--- src/app/api/Insert-Hobbies.php
+++ src/app/api/Insert-Hobbies.php
@@ -1,5 +1,5 @@
 <?php
- header("Access-Control-Request-Method: *");
+  header("Access-Control-Request-Method: *");
   header("Access-Control-Request-Headers: *");
   header("Access-Control-Allow-Origin: *");
 
@@ -8,50 +8,51 @@
   $obj = json_decode($json);
 
   $hobbyNameUpdate =$obj->HOBBYNAME;
+  $applicantEmail = $obj->APPLICANTEMAIL;
 
   // $hobbyNameUpdate ='booooo'; //testing
 // Create connection to Oracle
-$conn = oci_connect("ora_f5x0b", "a40858169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
-
-
+$conn = oci_connect("ora_e0w0b", "a22288161", "dbhost.ugrad.cs.ubc.ca:1522/ug");
 
 //TODO: add exception handling? (when the hobby is already in the database, will it crash? return error message?)
 
-$insert= "INSERT into Hobby values('$hobbyNameUpdate')";
+$insert1 = "INSERT INTO Hobby VALUES(:hobbyNameUpdate)";
+$insert2 = "INSERT INTO HaveHobby VALUES(:applicantEmail, :hobbyNameUpdate)";
 
 // $select= "SELECT hobbyName from Hobby where Hobby.hobbyName = 'booooo'"; //testing
 
-$stid2 = oci_parse($conn, $insert);
-// oci_bind_by_name($stid2, ':$hobbyNameUpdate', $hobbyNameUpdate); //dont need this to work
+$stid1 = oci_parse($conn, $insert1);
+oci_bind_by_name($stid1, ':hobbyNameUpdate', $hobbyNameUpdate);
+oci_execute($stid1);
 
-$result = oci_execute($stid2);
+$stid2 = oci_parse($conn, $insert2);
+oci_bind_by_name($stid2, ':applicantEmail', $applicantEmail);
+oci_bind_by_name($stid2, ':hobbyNameUpdate', $hobbyNameUpdate);
+oci_execute($stid2);
 
 // $stid = oci_parse($conn, $select); //testing
 // $result = oci_execute($stid);
 
+// //to remember the entire json string
+// $json = array(); //hailin
+
+// // Fetch each row in an associative array
+// //print '<table border="1">'
+
+// while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+
+//      //to concatenate a ',' after every json object. used to split before parsing.
+// 	    $json [] = $row ;
+
+// }
+// $response = (json_encode($json));
 
 
-//to remember the entire json string
-$json = array(); //hailin
-
-// Fetch each row in an associative array
-//print '<table border="1">'
-
-while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-
-     //to concatenate a ',' after every json object. used to split before parsing.
-	    $json [] = $row ;
-
-}
-$response = (json_encode($json));
-
-
-echo $response;
+// echo $response;
 
 // Fetch each row in an associative array
 
 //0cy27
-
 
 OCICommit($conn);
 oci_close($conn);
