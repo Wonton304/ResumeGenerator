792084520942ee315b58ee810ef7d97a8842a685
anlinpanda <anlin_c@yahoo.ca>
2018-06-17 01:17:56 -0700

    added updateJP.php

---
+++ src/app/api/updateJP.php
@@ -0,0 +1,51 @@
+<?php
+
+
+header("Access-Control-Request-Method: *");
+header("Access-Control-Request-Headers: *");
+header("Access-Control-Allow-Origin: *");
+
+$json = $HTTP_RAW_POST_DATA;
+echo $json;
+$obj = json_decode($json);
+$field = $obj->FIELD;
+$positionTitle = $obj->POSITIONTITLE;
+$id = $obj->ID;
+$description = $obj->DESCRIPTION;
+
+
+// Create connection to Oracle
+$conn = oci_connect("ora_e0w0b", "a22288161", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+
+$update1 = "UPDATE JobPostingPosts
+            SET field = :field, positionTitle = :positionTitle, description = :description
+            WHERE id = :id";
+
+
+$stid1 = oci_parse($conn, $update1);
+oci_bind_by_name($stid1, ':field', $field);
+oci_bind_by_name($stid1, ':positionTitle', $positionTitle);
+oci_bind_by_name($stid1, ':description', $description);
+oci_bind_by_name($stid1, ':id', $id);
+oci_execute($stid1);
+
+
+// //to remember the entire json string
+// $jsonresp = array();
+
+// // Fetch each row in an associative array
+// //print '<table border="1">'
+
+// while ($row = oci_fetch_array($stid2, OCI_RETURN_NULLS+OCI_ASSOC)) {
+
+//      //to concatenate a ',' after every json object. used to split before parsing.
+// 	    $jsonresp [] = $row ;
+
+// }
+
+// $response = (json_encode($jsonresp));
+
+
+// echo $response;
+
+?>
