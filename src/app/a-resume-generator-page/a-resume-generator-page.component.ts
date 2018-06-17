f086e3df2e4250fa8fd4537e921896816419335f
James Kan <james0077721@hotmail.com>
2018-06-17 02:04:57 -0700

    Update A lot of stuff

--- src/app/a-resume-generator-page/a-resume-generator-page.component.ts
+++ src/app/a-resume-generator-page/a-resume-generator-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from '../data-service.service';
+
 
 @Component({
   selector: 'app-a-resume-generator-page',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AResumeGeneratorPageComponent implements OnInit {
 
-  constructor() { }
+  jobPostingId:string = "";
+  generatedResume$:Object;
+
+  constructor(private serverService: DataServiceService) { }
 
   ngOnInit() {
   }
 
+  collectId (event:any){
+    this.jobPostingId=event.target.value;
+  }
+
+  // calls script
+  /*resumeGen (){
+    this.serverService.generateResume(JSON.stringify({JOBID:this.jobPostingId}))
+        .subscribe(
+          serverService => this.generatedResume$ = serverService,
+          (error) => console.log(error),
+        );
+
+        console.log(JSON.stringify({JOBID:this.jobPostingId}));
+        console.log("thisisresult:" + this.generatedResume$);
+  }
+*/
 }
--- src/app/data-service.service.ts
+++ src/app/data-service.service.ts
@@ -5,20 +5,26 @@ import { Http , Headers} from '@angular/http';
 @Injectable()
 export class DataServiceService {
 
-  constructor(private httpClient: HttpClient
-  , private http: Http) { }
-
+  constructor(private httpClient: HttpClient, private http: Http) { }
 
   connectJobPostings(){
     return this.httpClient.get('http://www.ugrad.cs.ubc.ca/~s3z0b/ServerConnect.php');
   }
 
-
-
   updateJobDescription(description){
-    const headers = new Headers ({'Content-Type': 'application/json'});
     return this.http.post('http://www.ugrad.cs.ubc.ca/~s3z0b/test.php', description);
   }
 
+  connectApplicants(postId){
+    return this.httpClient.post('http://www.ugrad.cs.ubc.ca/~s3z0b/applicantSelecting.php', postId);
+  }
 
+  generateResume (postID){
+    //return this.http.post('http://www.ugrad.cs.ubc.ca/~s3z0b/.php', postID);
+  }
+
+  createHobby(hobbyName){
+    return this.httpClient.post('http://www.ugrad.cs.ubc.ca/~s3z0b/hobby.php', hobbyName);
+  }
+  
 }
