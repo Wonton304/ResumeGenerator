d515b63814f4ebaf51926a2f3c0ba17e6bb14d95
amymao1 <mao.amy1@gmail.com>
2018-06-17 17:45:06 -0700

    loginpageapplicant php changed

--- src/app/api/loginPageApplicant.php
+++ src/app/api/loginPageApplicant.php
@@ -6,97 +6,105 @@ header("Access-Control-Allow-Origin: *");
 $json = $HTTP_RAW_POST_DATA;
 
 $obj = json_decode($json);
-<<<<<<< HEAD
+
 $json='';
 echo $json;
 
-$applicantName = $obj->NAME;
-$applicantEmail = $obj->EMAIL;
-$applicantAddress = $obj->ADDRESS;
-$applicantCity = $obj->CITY;
-$githubAccount = $obj->GITHUB;
-=======
-$applicantName = $obj->APPLICANTNAME;
-$applicantEmail = $obj->APPLICANTEMAIL;
-$applicantAddress = $obj->APPLICANTADDRESS;
-$applicantCity = $obj->APPLICANTCITY;
-$applicantProvince = $obj->APPLICANTPROVINCE;
-$githubAccount = $obj->GITHUBACCOUNT;
->>>>>>> a463b4a5bb043e2823447df39313fdcf1ae1deef
-$personalDescription = $obj->PERSONDESCRIPTION;
-$phoneNumber = $obj->APPLICANTPHONENUMBER;
+// $applicantPhoneNumber = $obj->APPLICANTPHONENUMBER;
+// $applicantName = $obj->APPLICANTNAME;
+// $applicantEmail = $obj->APPLICANTEMAIL;
+// $applicantAddress = $obj->APPLICANTADDRESS;
+// $applicantCity = $obj->APPLICANTCITY;
+// $applicantProvince = $obj->APPLICANTPROVINCE;
+// $githubAccount = $obj->GITHUBACCOUNT;
+// $personalDescription = $obj->PERSONDESCRIPTION;
+
+
+$applicantPhoneNumber = 'flob';
+$applicantName = 'blob';
+$applicantEmail = 'blob';
+$applicantAddress = 'blob';
+$applicantCity = 'blob';
+$applicantProvince = 'blob';
+$githubAccount = 'blob';
+$personalDescription = 'blob';
 
 // Create connection to Oracle
 $conn = oci_connect("ora_f5x0b", "a40858169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
 
-$insert1 = "INSERT INTO Applicant1 VALUES(:phoneNumber:, :applicantCity)";
+$insert1 = "INSERT INTO Applicant1 VALUES('$applicantPhoneNumber', '$applicantCity')";
 
-$insert6 = "INSERT INTO Applicant2 VALUES(applicantName, :applicantEmail, :applicantAddress,
-:githubAccount, :personalDescription, :phoneNumber)";
+// $insert1 = "INSERT INTO Applicant1 VALUES('hello', 'bye')";
 
-$insert7 = "INSERT INTO Applicant3 VALUES(:phoneNumber, :applicantProvince)";
 
-$insert2 = "INSERT INTO HaveHobby VALUES(:applicantEmail)";
+$insert6 = "INSERT INTO Applicant2 VALUES('$applicantName', '$applicantEmail', '$applicantAddress',
+'$githubAccount', '$personalDescription', '$applicantPhoneNumber')";
 
-$insert3 = "INSERT INTO HaveAward VALUES(:applicantEmail)";
+$insert7 = "INSERT INTO Applicant3 VALUES('$applicantPhoneNumber', '$applicantProvince')";
 
-$insert4 = "INSERT INTO HaveExperience VALUES(:applicantEmail)";
-
-$insert5 = "INSERT INTO HaveProject VALUES(:applicantEmail)";
+// $insert2 = "INSERT INTO HaveHobby VALUES('$applicantEmail')";
+//
+// $insert3 = "INSERT INTO HaveAward VALUES('$applicantEmail')";
+//
+// $insert4 = "INSERT INTO HaveExperience VALUES('$applicantEmail')";
+//
+// $insert5 = "INSERT INTO HaveProject VALUES('$applicantEmail')";
 
 // $select = "SELECT * FROM Company WHERE Company.companyName = 'Google'";
 
 $stid1 = oci_parse($conn, $insert1);
-oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
-oci_bind_by_name($stid1, ':applicantCity', $applicantCity);
+// oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
+// oci_bind_by_name($stid1, ':applicantCity', $applicantCity);
 oci_execute($stid1);
 
 $stid6 = oci_parse($conn, $insert6);
-oci_bind_by_name($stid1, ':applicantName', $applicantName);
-oci_bind_by_name($stid1, ':applicantEmail', $applicantEmail);
-oci_bind_by_name($stid1, ':applicantAddress', $applicantAddress);
-oci_bind_by_name($stid1, ':githubAccount', $githubAccount);
-oci_bind_by_name($stid1, ':personalDescription', $personalDescription);
-oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
+oci_execute($stid6);
+// oci_bind_by_name($stid1, ':applicantName', $applicantName);
+// oci_bind_by_name($stid1, ':applicantEmail', $applicantEmail);
+// oci_bind_by_name($stid1, ':applicantAddress', $applicantAddress);
+// oci_bind_by_name($stid1, ':githubAccount', $githubAccount);
+// oci_bind_by_name($stid1, ':personalDescription', $personalDescription);
+// oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
 
 $stid7 = oci_parse($conn, $insert7);
-oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
-oci_bind_by_name($stid1, ':applicantProvince', $applicantProvince);
+oci_execute($stid7);
+// oci_bind_by_name($stid1, ':phoneNumber', $phoneNumber);
+// oci_bind_by_name($stid1, ':applicantProvince', $applicantProvince);
 
-$stid2 = oci_parse($conn, $insert2);
-oci_bind_by_name($stid2, ':applicantEmail', $applicantEmail);
-oci_execute($stid2);
-
-$stid3 = oci_parse($conn, $insert3);
-oci_bind_by_name($stid3, ':applicantEmail', $applicantEmail);
-oci_execute($stid3);
-
-$stid4 = oci_parse($conn, $insert4);
-oci_bind_by_name($stid4, ':applicantEmail', $applicantEmail);
-oci_execute($stid4);
-
-$stid5 = oci_parse($conn, $insert5);
-oci_bind_by_name($stid5, ':applicantEmail', $applicantEmail);
-oci_execute($stid5);
-
-
-// //to remember the entire json string
-// $jsonresp = array();
-
-// // Fetch each row in an associative array
-// //print '<table border="1">'
-
-// while ($row = oci_fetch_array($stid2, OCI_RETURN_NULLS+OCI_ASSOC)) {
-
-//      //to concatenate a ',' after every json object. used to split before parsing.
-// 	    $jsonresp [] = $row ;
-
-// }
-
-// $response = (json_encode($jsonresp));
+// $stid2 = oci_parse($conn, $insert2);
+// // oci_bind_by_name($stid2, ':applicantEmail', $applicantEmail);
+// oci_execute($stid2);
+//
+// $stid3 = oci_parse($conn, $insert3);
+// // oci_bind_by_name($stid3, ':applicantEmail', $applicantEmail);
+// oci_execute($stid3);
+//
+// $stid4 = oci_parse($conn, $insert4);
+// // oci_bind_by_name($stid4, ':applicantEmail', $applicantEmail);
+// oci_execute($stid4);
+//
+// $stid5 = oci_parse($conn, $insert5);
+// // oci_bind_by_name($stid5, ':applicantEmail', $applicantEmail);
+// oci_execute($stid5);
 
 
-// echo $response;
+//to remember the entire json string
+$jsonresp = array();
+
+// Fetch each row in an associative array
+//print '<table border="1">'
+
+while ($row = oci_fetch_array($stid2, OCI_RETURN_NULLS+OCI_ASSOC)) {
+
+     //to concatenate a ',' after every json object. used to split before parsing.
+	    $jsonresp [] = $row ;
+
+}
+
+$response = (json_encode($jsonresp));
+
+
+echo $response;
 
 OCICommit($conn);
 oci_close($conn);
