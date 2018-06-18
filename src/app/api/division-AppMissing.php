db9b62d21e7e70481461112eedd1e63ad0bb8e37
amymao1 <mao.amy1@gmail.com>
2018-06-17 22:56:44 -0700

    fixed allAppAttributes

--- src/app/api/division-AppMissing.php
+++ src/app/api/division-AppMissing.php
@@ -9,18 +9,28 @@ $obj = json_decode($json);
 $json='';
 echo $json;
 
+$applicantEmailDiv = $obj->APPLICANTEMAIL;
+$jobidDiv = $obj->ID;
+
 // Create connection to Oracle
-$conn = oci_connect("ora_s3z0b", "a16599169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
+$conn = oci_connect("ora_f5x0b", "a40858169", "dbhost.ugrad.cs.ubc.ca:1522/ug");
 
 
 
-$aggregate = 'SELECT min(complexity) FROM CodingProject1';
-$stid = oci_parse($conn, $aggregate);
+$division= "SELECT R.technologyName
+            from Requires R
+            where R.id =:jobidDiv
+            intersect
+            select distinct H.language
+            from HaveProject H
+            where H.applicantEmail =: applicantEmailDiv";
+
+
+$stid = oci_parse($conn, $division);
 $result = oci_execute($stid);
-//to remember the entire json string
-$json = array();
-// Fetch each row in an associative array
-//print '<table border="1">'
+oci_bind_by_name($stid, ':jobidDiv', $jobidDiv);
+oci_bind_by_name($stid, ':applicantEmailDiv', $applicantEmailDiv);
+
 
 while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
      //to concatenate a ',' after every json object. used to split before parsing.
@@ -28,6 +38,7 @@ while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
 }
 $response = (json_encode($json));
 echo $response;
+
 // Fetch each row in an associative array
 
 //0cy27
